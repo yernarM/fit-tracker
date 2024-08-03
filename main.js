a43e8bed6ea4d5c0462feebcc5108cc2258c5086
yernarM <yernar.makhutov@mail.ru>
2024-08-03 19:31:47 +0500

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('.record')
+const button = document.querySelector('.add')
+const tbody = document.querySelector('.records')
+const actionCell = tbody.querySelector('.actions')
+
+button.addEventListener('click', add)
+
+function add() {
+	const record = input.value.trim()
+	const cell = tbody.insertRow().insertCell()
+
+	cell.append(record)
+	cell.after(actionCell.cloneNode(true))
+
+	input.value = ''
+}
