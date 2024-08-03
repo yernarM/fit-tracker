fe709b2afe98435cc129d3d6f339a5d45be7c092
yernarM <yernar.makhutov@mail.ru>
2024-08-03 19:43:24 +0500

    use spaces

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ const actionCell = tbody.querySelector('.actions')
 button.addEventListener('click', add)
 
 function add() {
-	const record = input.value.trim()
-	const cell = tbody.insertRow().insertCell()
+    const record = input.value.trim()
+    const cell = tbody.insertRow().insertCell()
 
-	cell.append(record)
-	cell.after(actionCell.cloneNode(true))
+    cell.append(record)
+    cell.after(actionCell.cloneNode(true))
 
-	input.value = ''
+    input.value = ''
 }
